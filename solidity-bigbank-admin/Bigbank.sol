5e2c5f96fc5fde1a9dcf18688c4a4fb69c702bd9
Luneihuahuo <3201863458li@gmail.com>
2025-07-11 21:52:04 +0800

    Create Bigbank.sol

---
+++ solidity-bigbank-admin/Bigbank.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "solidity-bigbank-admin/Bank.sol";
+
+contract BigBank is Bank {
+    address public admin;
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin");
+        _;
+    }
+
+    modifier depositLimit() {
+        require(msg.value > 0.001 ether, "Minimum 0.001 ETH");
+        _;
+    }
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function deposit() public payable override depositLimit() {
+        super.deposit();
+    }
+
+    function changeAdmin(address newAdmin) public onlyAdmin() {
+        require(newAdmin != address(0), "Invalid address");
+        admin = newAdmin;
+    }
+}
